aff62ef02b526fee727a29e297365bbf4bf427a1
Brian Wong <brianw7218@gmail.com>
2023-04-10 12:37:39 -0700

    Render dummy comments

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Dingus",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Pingus",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Odin Message Board", messages });
 });
 
 module.exports = router;
