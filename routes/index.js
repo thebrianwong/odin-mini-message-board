68232b55e65d667c042cf06d2051c77ad11d55fd
Brian Wong <brianw7218@gmail.com>
2023-04-10 14:04:18 -0700

    Add ability to add new comments

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,18 @@ router.get("/", (req, res, next) => {
   res.render("index", { title: "Odin Message Board", messages });
 });
 
+router.get("/new", (req, res) => {
+  res.render("form");
+});
+
+router.post("/new", (req, res) => {
+  const newMessage = {
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  };
+  messages.push(newMessage);
+  res.redirect("/");
+});
+
 module.exports = router;
